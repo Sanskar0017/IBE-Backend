931092b54207ef0f7866f06e504932a2d2b6cc69
Sanskar0017 <sanskarkumar.saraf@kickdrumtech.com>
2024-03-06 17:36:45 +0530

    Spring Initialization

---
+++ src/main/java/com/team14/IBEbackend/IbEbackendApplication.java
@@ -0,0 +1,13 @@
+package com.team14.IBEbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbEbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbEbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team14/IBEbackend/IbEbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team14.IBEbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbEbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

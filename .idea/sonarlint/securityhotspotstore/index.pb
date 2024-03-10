a2e3f345c319095f7c5c8d3c388b44177aa12080
Sanskar0017 <sanskarkumar.saraf@kickdrumtech.com>
2024-03-10 23:30:29 +0530

    Approval Commits - Endpoint URL

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -4,12 +4,10 @@
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 o
-?src/main/java/com/team14/IBE/TestController/TestController.java,c/2/c262b86cf3a333c5c692d61bda867e35e4b468b4
-e
-5src/main/java/com/team14/IBE/Service/TestService.java,0/d/0da2f808b6bb2f3ef54c923d15617eb98604d598
-o
 ?src/main/java/com/team14/IBE/Service/GraphQLTestingService.java,1/7/1789fa19ef1076c5c0488cd63aab1e43b1f8c412
 r
 Bsrc/main/java/com/team14/IBE/TestController/GraphQLController.java,e/3/e385d4b5960ec5ef7818898f2e2bca13afc8ceaf
 e
 5src/test/java/com/team14/IBE/IbeApplicationTests.java,f/a/fab3ec7b28fc8fec55a669defb0bf72dc7e3b6a0
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- src/main/java/com/team14/IBE/Service/GraphQLTestingService.java
+++ src/main/java/com/team14/IBE/Service/GraphQLTestingService.java
@@ -6,16 +6,21 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GraphQLTestingService {
+
     private static final String ROOMS_DATA = "{ listRooms {room_id room_number } }";
     @Value("${api.key}")
     private String apiKey;
+    @Value("${graphql.endpoint}")
+    private String graphqlEndpoint;
+
     private final RestTemplate restTemplate = new RestTemplate();
+
     public ResponseEntity<String> getRooms(){
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         httpHeaders.set("x-api-key", apiKey);
         String requestBody = "{ \"query\": \"" + ROOMS_DATA + "\" }";
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, httpHeaders);
-        return restTemplate.exchange("http://localhost:4000/graphql", HttpMethod.POST, requestEntity, String.class);
+        return restTemplate.exchange(graphqlEndpoint, HttpMethod.POST, requestEntity, String.class);
     }
 }
